2ca18dc2b9968b674ad5211a839bc6724500e49b
tuentt <tuentt@vng.com.vn>
2017-12-11 13:38:13 +0700

    Done download 1 file

--- API_GetPdfProfiles.js
+++ API_GetPdfProfiles.js
@@ -1,25 +1,57 @@
-var req = new XMLHttpRequest();
-req.open("GET", "https://www.linkedin.com/dms/C5100AQEDLOI9J35ntA/profile-profilePdf/0?m=367667054&e=1513051307&v=alpha&t=OZmC75B7N8bUs4OguqY4EunXRucTbBRCkRlJQKC5IDA");
-req.responseType = "blob";
-req.onreadystatechange = function () {
-    if (req.readyState === 4 && req.status === 200) {
-        var filename = "PdfName-" + new Date().getTime() + ".pdf";
-        if (typeof window.chrome !== 'undefined') {
-            // Chrome version
-            var link = document.createElement('a');
-            link.href = window.URL.createObjectURL(req.response);
-            link.download = "PdfName-" + new Date().getTime() + ".pdf";
-            link.click();
-        } else if (typeof window.navigator.msSaveBlob !== 'undefined') {
-            // IE version
-            var blob = new Blob([req.response], { type: 'application/pdf' });
-            window.navigator.msSaveBlob(blob, filename);
-        } else {
-            // Firefox version
-            var file = new File([req.response], filename, { type: 'application/force-download' });
-            window.open(URL.createObjectURL(file));
+function getPDFProfile(dmsLink, idProfile) {
+    var req = new XMLHttpRequest();
+    req.open("GET", dmsLink);
+    var namePDF = decodeURI(idProfile);
+    console.log(namePDF);
+    req.responseType = "blob";
+    req.onreadystatechange = function () {
+        if (req.readyState === 4 && req.status === 200) {
+            var filename = namePDF + ".pdf";
+            if (typeof window.chrome !== 'undefined') {
+                // Chrome version
+                var link = document.createElement('a');
+                link.href = window.URL.createObjectURL(req.response);
+                link.download = namePDF + ".pdf";
+                link.click();
+            } else if (typeof window.navigator.msSaveBlob !== 'undefined') {
+                // IE version
+                var blob = new Blob([req.response], {type: 'application/pdf'});
+                window.navigator.msSaveBlob(blob, filename);
+            } else {
+                // Firefox version
+                var file = new File([req.response], filename, {type: 'application/force-download'});
+                window.open(URL.createObjectURL(file));
+            }
         }
-    }
-};
+    };
+    req.send();
+}
 
-req.send();
+// send POST pdf profile
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift().replace('"', '').replace('"', '');
+}
+
+var csrfToken = getCookie('JSESSIONID');
+
+// send POST pdf profile
+function POSTRequest(idProfile) {
+    var linkRequest = "https://www.linkedin.com/voyager/api/identity/profiles/" + idProfile + "/profileActions?versionTag=3123833624&action=saveToPdf";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            var parsedData = JSON.parse(this.responseText);
+            // console.log(parsedData.value);
+            getPDFProfile(parsedData.value, idProfile);
+        }
+    };
+    xhttp.open("POST", linkRequest, true);
+    xhttp.setRequestHeader("csrf-token", csrfToken);
+    xhttp.send();
+}
+
+POSTRequest("nguy%E1%BB%85n-thanh-nh%C3%A0n-034398120");
+
