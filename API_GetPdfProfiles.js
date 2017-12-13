cea3c8240fad03f5274485b3a3a79a10883a7a1a
tuentt <tuentt@vng.com.vn>
2017-12-13 11:26:48 +0700

    Done - get profile

--- API_GetPdfProfiles.js
+++ API_GetPdfProfiles.js
@@ -1,27 +1,62 @@
-let profileId = ["nguyenta", "christophelefebvre", "nghia-doan-a5402534"];
+var profileId = ["carwyn-littlewood-0b394655", "sultan-ahmed-959b6355", "nguyentoanngoc", "ricardoglenn", "nguyen-truc-6aa0586", "bang-ngo-1a212b6", "kentran27", "chon-nguyen-huu-b0b3396", "huanduong", "trung-le-thanh-22a5316"];
+
+function removeA(arr) {
+    var what, a = arguments, L = a.length, ax;
+    while (L > 1 && arr.length) {
+        what = a[--L];
+        while ((ax = arr.indexOf(what)) !== -1) {
+            arr.splice(ax, 1);
+        }
+    }
+    return arr;
+}
+
+function exportFile(data, filename) {
+
+    if (!data) {
+        console.error('Console.save: No data');
+        return;
+    }
+
+    if (!filename) filename = 'console.json';
+
+    if (typeof data === "object") {
+        data = JSON.stringify(data, undefined, 4)
+    }
+
+    var blob = new Blob([data], {type: 'text/json'}),
+        e = document.createEvent('MouseEvents'),
+        a = document.createElement('a');
+
+    a.download = filename;
+    a.href = window.URL.createObjectURL(blob);
+    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    a.dispatchEvent(e)
+}
 
 function getPDFProfile(dmsLink, idProfile) {
 
-    let req = new XMLHttpRequest();
-    req.open("GET", dmsLink, false);
-    let namePDF = decodeURI(idProfile);
+    var req = new XMLHttpRequest();
+    req.open("GET", dmsLink, true);
+    var namePDF = decodeURI(idProfile);
     req.responseType = "blob";
-        req.onreadystatechange = function () {
+    req.onreadystatechange = function () {
         if (req.readyState === 4 && req.status === 200) {
-            let filename = namePDF + ".pdf";
+            var filename = namePDF + ".pdf";
             if (typeof window.chrome !== 'undefined') {
                 // Chrome version
-                let link = document.createElement('a');
+                var link = document.createElement('a');
                 link.href = window.URL.createObjectURL(req.response);
                 link.download = namePDF + ".pdf";
                 link.click();
             } else if (typeof window.navigator.msSaveBlob !== 'undefined') {
                 // IE version
-                let blob = new Blob([req.response], {type: 'application/pdf'});
+                var blob = new Blob([req.response], {type: 'application/pdf'});
                 window.navigator.msSaveBlob(blob, filename);
             } else {
                 // Firefox version
-                let file = new File([req.response], filename, {type: 'application/force-download'});
+                var file = new File([req.response], filename, {type: 'application/force-download'});
                 window.open(URL.createObjectURL(file));
             }
         }
@@ -29,39 +64,89 @@ function getPDFProfile(dmsLink, idProfile) {
     req.send();
 }
 
+var dmsLink = {};
+
 // send POST pdf profile
 function getCookie(name) {
-    let value = "; " + document.cookie;
-    let parts = value.split("; " + name + "=");
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
     if (parts.length === 2) return parts.pop().split(";").shift().replace('"', '').replace('"', '');
 }
 
-let csrfToken = getCookie('JSESSIONID');
-let succeedGetProfile = [];
-let failGetProfile = [];
+var csrfToken = getCookie('JSESSIONID');
+var succeedGetProfile = [];
+var failGetProfile = [];
 
 // send POST pdf profile
 function POSTRequest(idProfile) {
-    let linkRequest = "https://www.linkedin.com/voyager/api/identity/profiles/" + idProfile + "/profileActions?versionTag=3123833624&action=saveToPdf";
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            let parsedData = JSON.parse(this.responseText);
-            getPDFProfile(parsedData.value, idProfile)
+    return new Promise(resolve => {
+        var linkRequest = "https://www.linkedin.com/voyager/api/identity/profiles/" + idProfile + "/profileActions?versionTag=3123833624&action=saveToPdf";
+        var xhttp = new XMLHttpRequest();
+
+        function reqListener() {
+            resolve(JSON.parse(this.responseText));
+            if (this.readyState === 4 && this.status === 200) {
+                var parsedData = JSON.parse(this.responseText);
+                dmsLink[idProfile] = parsedData.value;
+                // getPDFProfile(parsedData.value, idProfile)
+                var index = failGetProfile.indexOf(idProfile);
+                if (index > -1) {
+                    console.log("slice: " + index, idProfile);
+                    // failGetProfile.slice(index, 1);
+                    //
+                    removeA(failGetProfile, idProfile);
+                    console.log(failGetProfile);
+                }
+            } else {
+                if (failGetProfile.indexOf(idProfile) === -1) {
+                    failGetProfile.push(idProfile);
+                }
+            }
         }
-    };
-    xhttp.open("POST", linkRequest, false);
-    xhttp.setRequestHeader("csrf-token", csrfToken);
-    xhttp.send();
+
+        xhttp.addEventListener("load", reqListener);
+        xhttp.open("POST", linkRequest, true);
+        xhttp.setRequestHeader("csrf-token", csrfToken);
+        xhttp.send();
+    });
+
+}
+
+var numberRecall = 1;
+var limitRecall = 5;
+
+function recallGetProfile() {
+    console.log("numberRecall: " + numberRecall);
+    numberRecall += 1;
+    var failPromies = [];
+    failGetProfile.forEach(function (profile_fail) {
+        failPromies.push(POSTRequest(profile_fail));
+    });
+    Promise.all(failPromies).then(function () {
+        console.log("Fail- : " + failGetProfile);
+        if (failGetProfile.length > 0 && numberRecall <= limitRecall) {
+            recallGetProfile(failGetProfile);
+        }
+        if (numberRecall > limitRecall && failGetProfile.length > 0) {
+            //save id cannot GetProfile to file pdf
+            exportFile(failGetProfile.toString(), "profileID_Failed.txt")
+        }
+        if (numberRecall > limitRecall || failGetProfile.length <= 0) {
+            for (var profile_id in dmsLink) {
+                getPDFProfile(dmsLink[profile_id], profile_id);
+            }
+        }
+    });
 }
 
 function main() {
+    var promises = [];
     profileId.forEach(function (entry) {
-        POSTRequest(entry);
+        promises.push(POSTRequest(entry));
     });
-    console.log("Done");
-    console.log("ss " + succeedGetProfile);
-    console.log("fail " + failGetProfile);
+    Promise.all(promises).then(function () {
+        recallGetProfile();
+    });
 }
 
 main();
--- test.js
+++ test.js
@@ -1,62 +1,24 @@
-var profileId = ["Tags"];
-function getPDFProfile(dmsLink, idProfile) {
-    var req = new XMLHttpRequest();
-    req.open("GET", dmsLink);
-    var namePDF = decodeURI(idProfile);
-    console.log(namePDF);
-    req.responseType = "blob";
-    req.onreadystatechange = function () {
-        if (req.readyState === 4 && req.status === 200) {
-            var filename = namePDF + ".pdf";
-            if (typeof window.chrome !== 'undefined') {
-                // Chrome version
-                var link = document.createElement('a');
-                link.href = window.URL.createObjectURL(req.response);
-                link.download = namePDF + ".pdf";
-                link.click();
-            } else if (typeof window.navigator.msSaveBlob !== 'undefined') {
-                // IE version
-                var blob = new Blob([req.response], {type: 'application/pdf'});
-                window.navigator.msSaveBlob(blob, filename);
-            } else {
-                // Firefox version
-                var file = new File([req.response], filename, {type: 'application/force-download'});
-                window.open(URL.createObjectURL(file));
-            }
-        }
-    };
-    req.send();
+
+function exportFile(data, filename) {
+
+    if (!data) {
+        console.error('Console.save: No data');
+        return;
+    }
+
+    if (!filename) filename = 'console.json';
+
+    if (typeof data === "object") {
+        data = JSON.stringify(data, undefined, 4)
+    }
+
+    var blob = new Blob([data], {type: 'text/json'}),
+        e = document.createEvent('MouseEvents'),
+        a = document.createElement('a');
+
+    a.download = filename;
+    a.href = window.URL.createObjectURL(blob);
+    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    a.dispatchEvent(e)
 }
-
-// send POST pdf profile
-function getCookie(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
-    if (parts.length == 2) return parts.pop().split(";").shift().replace('"', '').replace('"', '');
-}
-
-var csrfToken = getCookie('JSESSIONID');
-console.log(csrfToken);
-
-// send POST pdf profile
-function POSTRequest(idProfile) {
-    var linkRequest = "https://www.linkedin.com/voyager/api/identity/profiles/" + idProfile + "/profileActions?versionTag=3123833624&action=saveToPdf";
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-
-            var parsedData = JSON.parse(this.responseText);
-            // console.log(parsedData.value);
-            getPDFProfile(parsedData.value, idProfile);
-        }
-    };
-    xhttp.open("POST", linkRequest, true);
-    xhttp.setRequestHeader("csrf-token", csrfToken);
-    xhttp.send();
-}
-
-
-
-profileId.forEach(function(entry) {
-    POSTRequest(entry);
-});
