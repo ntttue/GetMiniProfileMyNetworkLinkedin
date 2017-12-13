ebb09f170b0fc17fe1dd78f8d67e301a81f3c943
tuentt <tuentt@vng.com.vn>
2017-12-13 14:11:59 +0700

    Update final

--- API_GetPdfProfiles.js
+++ API_GetPdfProfiles.js
@@ -1,4 +1,4 @@
-var profileId = ["carwyn-littlewood-0b394655", "sultan-ahmed-959b6355", "nguyentoanngoc", "ricardoglenn", "nguyen-truc-6aa0586", "bang-ngo-1a212b6", "kentran27", "chon-nguyen-huu-b0b3396", "huanduong", "trung-le-thanh-22a5316"];
+var profileId = ["huyns","andytranhr","angiestrahle","kiettran2502","anh-mai-vu-ba0a9488","an-le-5ab37956","an-phan-44878ba2","bang-truong-cong-57bb624b","tuan-bui-12186398","phong-nguyen-ba-45a8921b","vu-bui-47b8511a","buidanglong","duyet-pham-5ab26272","cuong-cao-7149a271","chatchai-totamkead-b90a5b95","cathy-tran-41b4b012a","chi-pham-88434b38","thu-huynh-47305564","chinh-nguyen-a36b57a9","chu-cong-chinh-1a081154","nhi-huynh-5311ab67","tran-cong-dung-hanh-32b80b99","cuongchu","cuong-lam-quoc-37944264","tuan-dang-30a14834","phung-dang-6b07b1a3","danh-nguyen-7316b825","dan-thanh-168a6833","huy-dien-quang-014105ab","ngoc-khanh-ha-dinh-a3610644","minh-tran-dinh-82727919","doana1988","trang-do-baa1b187","dungdaotuan","dung-hoang-quoc-63b3035","duong-huy-1ab022b6","duong-minh-lam-113007b2","trang-duong-656213b9","duynghiax","lisa-cao-59296947","giang-chu-a38bb059","chi-huynh-947566a4","hac-huynh-229a8633","tam-nguyen-a2058220","hanh-thai-7a64a524","hao-tran-9a377660","hiep-nguyen-524767138","vu-tran-henry-03a21139","hien-nguyen-175049a6","martinhiennguyen","hien-trinh-57674a107","hiep-nguyen-hoang-6b721672","huynhdung","ngoc-nguyen-242177116","yennguyenamwayvn","zingdang","hthtrung","cuong-hoang-quoc-28100364","han-huynh-94a45522","hung-tran-le-a5699726","hung-dao-69204016","khanhhung","hung-nguyen-thanh-14700426","lan-bui-51b35979","trang-luu-11660031","huy-anh-865a5693","huylyanh","huynh-thi-chinh-5b98b369","hao-huynh-phuoc-a1b69070","vy-nguyen-8a874119","yennguyenamwayvn","james-wong-5bb0b199","vu-nguyen-5847908a","duyen-truong-1b622889","kieu-manh-giap-lawyer-0b44b3112","kim-h-thai-86433464","nhatbui29","lan-nguyen-a21b6184","thai-khuong-lam-06520289","ngoc-lam-08777883","phung-linh-la-b3b66a4b","huy-le-quang-990406a","leduyenan","lethianhnhan","nhungle0803","le-kim-thu-33163935","lelamthuyphuong","lenhantin2005","chau-le-7b826191","linh-nguyen-hoang-115164a6","linh-coach-51483389","nhhlinh","khuyen-le-25807a98","ngtuonghuan","binh-le-b58a95125","chau-truong-18429354","hang-nga-luu-a0b561136","quyen-ly-4a908319","thuy-ho-792483146","trang-mai-6a53b187","huyentra","truc-mai-26948018","jessica-mai-vinh-truc-32618563","manh-khuu-michelle-a61a3191","mechu-g-6a155013a","huynh-minh-hai-83b9989b","nhut-nguyen-hoang-minh","huynh-minh-tam-nguyen-71248a65","tram-tran-62ab03147","yen-dinh-a409b557","thuy-hoang-658794a2","ltmphung","minh-nguyen-2b211695","nam-nguyen-1354ab78","han-hoang-ngoc-bb0b1228","trung-ta-quang-1165239a","trang-nguyen-03a691a8","tran-thi-nga-40147166","ngan-dang-43676479","le-sang-nguyen-0b2215b3","thuy-dung-ngo-7452754a","ngo-binh-9a30459","diem-nguyen-2a9918112","huong-nguyen-thi-ngoc-0926b832","phung-vu-121a216b","nguyen-tan-bb7869b1","ngoc-vo-6597831a","thao-ngo-756970104","thanh-nguyen-b12759136","ngsang032003","quan-nguyen-77a217b3","minh-nguyen-duc-618a36a8","trinh-nguyen-826218109","hanh-nguyen-bb387b27","hung-nguyen-961314a7","nguyenhuynhnhu","tuantu712","pham-van-nguyen-227179a1","nguyen-phu-huu-loc-a875b838","gent-nguyen-phuoc-toan-99094b23","trang-nguyen-jasmine-nguyen-29172527","nguyentrongkhuong","nguyenz0411","nguyet-huynh-5365a9120","vu-thi-nguyet-que-319731a1","nhan-tran-thanh-b4127254","megan-le-b19566a3","vanessa-xuan-48891025","hao-nguyen-7876b414","nhi-mai-815124116","nhsang","nhuquynh","lecuong-nguyen-881a3063","nhan-le-a2355853","nguyen-thi-quynh-anh-5a00601b","pham-doan-ket-4820bb14","minh-huy-pham-04b17339","nhat-pham-65560417","phamnhatthuong","yen-pham-a3150539","pham-vu-anh-479ba731","thong-phan-20958031","thy-phan-1772b370","pham-huu-tai-331b68a9","phung-khuu-a935a664","phuoc-nguyen-viet-b20a2852","phuong-la-shrm-cp-ccmc-2439b614","phuong-vu-5a255384","le-hoang-ha-phuong-35a60a138","phuong-thao-huynh-a5127a23","tranthiphuongthao","nguyen-thi-my-hoa-6b546854","khoa-phan-1a2a853","thinh-nguyen-0b2ba314","veeravit-puvaveeranin-65452393","quynh-nhan-ho-57077613","xuan-hoang-a4841b63","quan-tran-uy-64075475","bui-huu-quan-anh-617b3690","quoc-nguyen-b0389267","ngoc-nguyen-norem-34a9884b","nhi-la-ba56aa49","thao-hoang-8517435b","son-tran-68285613b","khang-le-0184b837","anh-le-64a31a106","su-hai-b95b6722","tam-tong-hoang-5809b239","thuy-dang-b552a428","thinhletan","tran-ta-56611427","thach-trat-47966a42","thai-ngo-333a2252","chris-nguyen-81208b35","dung-nguyen-b6523410b","tran-huong-757859b8","le-thanh-long-7a18833a","ngoc-tran-thanh-640961102","phanthithanhphuong","jasmine-nguyen-a8024b128","truc-pham-a52b2887","xuan-le-a05b2752","thinh-vu-673210b8","thuha-nguyen-242aa21b","seanhuynh","linh-pham-9b1368112","ngan-nguyen-566a6653","thuytiennguyen8487","lethithuytrang","tin-tran-a8268867","thanh-dang-xuan","thuy-tran-28a1626","huong-tran-b5417a74","nguyen-trang-5a1abb120","trang-lam","hung-tran-a599382a","khoi-tran-dinh-28968595","an-tran-83a29531","minh-tran-11420143","tran-si-lai-77670012","nguyenkienthinh","trieu-anh-jean-85b92363","uyen-trinh-thi-xuan-a9130b35","le-quang-trong-tin-a028a7b4","trung-nguyen-bao-6631567a","trung-nguyen-4806b195","tuanvo1986","aunguyen189","tuyen-pham-a54861b3","thy-le-b33b4154","uyen-bach-847747ba","anh-tuan-b8108474","van-mai-thuy-5bb6a498","van-thanh-9a138a75","anhmai307","van-cap-tai-4ba29ba2","hang-truong-2890b231","le-van-70470b38","taiz-nguyen-8b436252","truc-tran-305b3917","viet-le-26180437","vinh-doan-58182927","vinh-nghiem-the-04500171","yen-vo-a5930091","hung-vo-duy-ba34154a","vohuuthuan","vo-tai-thao-quyen-abb74499","nga-vo-181b7856","tuanvoba","lan-vo-thi-xuan-a1155a48","duy-nguyen-acca-cpa-b8812756","linh-vu-3b007911","vy-tran-51a21b120","thuy-le-576386ab","linh-diep-yen-6a389a13b","yennguyenamwayvn","my-ngo-59bb8474","zoeydinh"];
 
 function removeA(arr) {
     var what, a = arguments, L = a.length, ax;
@@ -14,7 +14,6 @@ function removeA(arr) {
 function exportFile(data, filename) {
 
     if (!data) {
-        console.error('Console.save: No data');
         return;
     }
 
@@ -91,11 +90,7 @@ function POSTRequest(idProfile) {
                 // getPDFProfile(parsedData.value, idProfile)
                 var index = failGetProfile.indexOf(idProfile);
                 if (index > -1) {
-                    console.log("slice: " + index, idProfile);
-                    // failGetProfile.slice(index, 1);
-                    //
                     removeA(failGetProfile, idProfile);
-                    console.log(failGetProfile);
                 }
             } else {
                 if (failGetProfile.indexOf(idProfile) === -1) {
@@ -113,25 +108,31 @@ function POSTRequest(idProfile) {
 }
 
 var numberRecall = 1;
-var limitRecall = 5;
+var limitRecall = 20;
+var isDownloadPDF = false;
+var isExportFailFile = false;
 
 function recallGetProfile() {
-    console.log("numberRecall: " + numberRecall);
+    console.log(numberRecall);
     numberRecall += 1;
     var failPromies = [];
     failGetProfile.forEach(function (profile_fail) {
         failPromies.push(POSTRequest(profile_fail));
     });
     Promise.all(failPromies).then(function () {
-        console.log("Fail- : " + failGetProfile);
         if (failGetProfile.length > 0 && numberRecall <= limitRecall) {
-            recallGetProfile(failGetProfile);
+            setTimeout(function(){
+                recallGetProfile();
+            }, 5000);
         }
-        if (numberRecall > limitRecall && failGetProfile.length > 0) {
+        if ((numberRecall > limitRecall && failGetProfile.length > 0) && isExportFailFile === false) {
             //save id cannot GetProfile to file pdf
+            isExportFailFile = true;
             exportFile(failGetProfile.toString(), "profileID_Failed.txt")
         }
-        if (numberRecall > limitRecall || failGetProfile.length <= 0) {
+        if ((numberRecall > limitRecall || failGetProfile.length <= 0) && isDownloadPDF === false) {
+            console.log("DONEEEEEEEEEEEEEEEEEEEEEEEEE");
+            isDownloadPDF = true;
             for (var profile_id in dmsLink) {
                 getPDFProfile(dmsLink[profile_id], profile_id);
             }
@@ -145,7 +146,9 @@ function main() {
         promises.push(POSTRequest(entry));
     });
     Promise.all(promises).then(function () {
-        recallGetProfile();
+        setTimeout(function(){
+            recallGetProfile();
+        }, 5000);
     });
 }
 
